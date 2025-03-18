39f136f72062c107ae019f67b61977a07c18afa6
mariusostby <ostbym@icloud.com>
2025-03-18 14:48:01 +0100

    fix cors

--- TF/FunctionApp/main.tf
+++ TF/FunctionApp/main.tf
@@ -64,22 +64,14 @@ resource "azurerm_linux_function_app" "example_function_app" {
     application_stack {
       powershell_core_version = "7.4"
     }
+    cors {
+      allowed_origins = ["portal.azure.com"]
+    }
   }
 
   identity {
     type = "SystemAssigned" # Use managed identity over access keys
   }
-}
-
-# allow portal in CORS to a
-resource "azurerm_app_service_cors" "function_cors" {
-  resource_group_name = azurerm_resource_group.rg_function_app.name
-  app_service_name    = azurerm_function_app.example_function_app.name
-
-  allowed_origins = [
-    "https://portal.azure.com",
-    # add additional origins if needed
-  ]
 }
 
 resource "azurerm_role_assignment" "function_app_role" {
