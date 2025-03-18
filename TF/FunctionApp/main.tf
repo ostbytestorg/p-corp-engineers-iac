797adc0efe694b57dc737ba56d76c7743f753782
Marius Østby <51474245+mariusostby@users.noreply.github.com>
2025-03-18 11:34:40 +0100

    add functionapp (#5)

--- TF/FunctionApp/main.tf
+++ TF/FunctionApp/main.tf
@@ -36,3 +36,37 @@ resource "azurerm_storage_blob" "storage_blob_function" {
   storage_container_name = "function-code"
   type                   = "Block"
 }
+
+resource "azurerm_service_plan" "function_app_service_plan" {
+  name                = "appplan-sicratffunc001"
+  location            = azurerm_resource_group.rg_function_app.location
+  resource_group_name = azurerm_resource_group.rg_function_app.name
+  os_type             = "Linux"
+  sku_name            = "P0v3"
+}
+
+resource "azurerm_linux_function_app" "example_function_app" {
+  name                          = "sicratffunc001"
+  location                      = azurerm_resource_group.rg_function_app.location
+  resource_group_name           = azurerm_resource_group.rg_function_app.name
+  service_plan_id               = azurerm_service_plan.function_app_service_plan.id
+  storage_account_name          = azurerm_storage_account.function_app_storage_account.name
+  storage_uses_managed_identity = true
+  public_network_access_enabled = true
+
+  app_settings = {
+    "AzureWebJobsStorage__accountName" = azurerm_storage_account.function_app_storage_account.name
+    "HASH"                             = base64encode(filesha256("./FUNCTION_APP_CODE.zip"))
+    "WEBSITE_RUN_FROM_PACKAGE"         = azurerm_storage_blob.storage_blob_function.url
+    "FUNCTIONS_WORKER_RUNTIME"         = "powershell"   # Set runtime to PowerShell
+    "FUNCTIONS_EXTENSION_VERSION"      = "~4"           # Recommended Functions runtime version
+  }
+
+  site_config {
+    always_on = true  # Prevents idle timeout (important for PowerShell triggers, timers, etc.)
+  }
+
+  identity {
+    type = "SystemAssigned"  # Use managed identity over access keys
+  }
+}
