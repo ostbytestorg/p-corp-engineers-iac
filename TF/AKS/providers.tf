ffdd057b1aa640e5bcb53efdee23a63f7d9a9fb6
Marius Østby <51474245+mariusostby@users.noreply.github.com>
2025-03-27 15:15:12 +0100

    Feat/deploy argo (#16)
    * add argo
    
    * try argo
    
    * try argo
    
    * try argo
    
    * try argo
    
    * try argo
    
    * try argo
    
    * add separate pipeline for k8s config
    
    * add separate pipeline for k8s config
    
    * add fedcred permissions
    
    * disable sync run on pr
    
    * fix kubelogin validate
    
    * try noninteractive
    
    * USE LATEST
    
    * UPDATE FLAG
    
    * UPDATE FLAG
    
    * add sp to admin group
    
    * config workflow dispatch only

--- TF/AKS/providers.tf
+++ TF/AKS/providers.tf
@@ -12,3 +12,10 @@ provider "azurerm" {
 
 provider "azuread" {
 }
+
+provider "kubernetes" {
+  host                   = azurerm_kubernetes_cluster.aks.kube_config.0.host
+  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_certificate)
+  client_key             = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks.kube_config.0.cluster_ca_certificate)
+}
