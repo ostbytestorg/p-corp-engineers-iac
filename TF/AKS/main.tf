6d0241ef94270673e96f596878f1ece5aad47b16
Marius Østby <51474245+mariusostby@users.noreply.github.com>
2025-03-27 10:17:43 +0100

    add aks (#13)
    * add aks
    
    * fix name references
    
    * fix pipe naming
    
    * add admin group config

---
+++ TF/AKS/main.tf
@@ -0,0 +1,78 @@
+terraform {
+  backend "azurerm" {}
+}
+
+data "azurerm_client_config" "current" {}
+
+# entra group for admins
+
+resource "azuread_group" "entra_admin_group" {
+  display_name = "grp-aks-admin"
+  security_enabled = true
+}
+
+# resourcegroup for aks and acr
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+# Create an Azure Container Registry for your images
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# Create an AKS Cluster
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "akspoc"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_B2s"  # Cost-efficient size for a POC.
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  azure_active_directory_role_based_access_control {
+    azure_rbac_enabled = true
+    admin_group_object_ids = [ azuread_group.entra_admin_group.id ]
+    tenant_id = data.azurerm_client_config.current.tenant_id
+  }
+
+}
+
+resource "azurerm_role_assignment" "acrtoaks" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  sensitive = true
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
---
+++ TF/AKS/terraform.tfvars
@@ -0,0 +1,5 @@
+location               = "norwayeast"
+resource_group_name    = "rg-tf-aks"
+tags                   = { configuration = "terraform" }
+aks_cluster_name = "aksostbyengineeering001"
+acr_name = "acrostbyengineering001"
