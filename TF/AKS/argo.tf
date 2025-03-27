523c52777d42e8ad669351f228ae15ffc1981d3e
mariusostby <mariusostby@sicra.no>
2025-03-27 13:59:00 +0100

    try argo

--- TF/AKS/argo.tf
+++
@@ -1,7 +0,0 @@
-data "http" "argocd_install" {
-  url = "https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml"
-}
-
-resource "kubernetes_manifest" "argocd" {
-  manifest = yamldecode(data.http.argocd_install.body)
-}
