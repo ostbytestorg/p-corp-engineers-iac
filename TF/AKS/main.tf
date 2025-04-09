8fd6386572133e3d37025f465d6dfb705899aba9
mariusostby <mariusostby@sicra.no>
2025-04-09 13:03:23 +0200

    use vars and indexes

--- TF/AKS/main.tf
+++ TF/AKS/main.tf
@@ -5,22 +5,22 @@ terraform {
 data "azurerm_client_config" "current" {}
 
 # entra group for admins
-
 resource "azuread_group" "entra_admin_group" {
-  count = var.deploy ? 1 : 0
-  display_name = "grp-aks-admin"
-  security_enabled = true
+  count               = var.deploy ? 1 : 0
+  display_name        = "grp-aks-admin"
+  security_enabled    = true
 }
 
 # Add the currently running service principal to the above group
 resource "azuread_group_member" "entra_admin_member" {
-  group_object_id  = azuread_group.entra_admin_group.object_id[0] 
+  count            = var.deploy ? 1 : 0
+  group_object_id  = azuread_group.entra_admin_group[0].object_id 
   member_object_id = data.azurerm_client_config.current.object_id
 }
 
 # resourcegroup for aks and acr
 resource "azurerm_resource_group" "rg" {
-  count = var.deploy ? 1 : 0
+  count    = var.deploy ? 1 : 0
   name     = var.resource_group_name
   location = var.location
   tags     = var.tags
@@ -28,7 +28,7 @@ resource "azurerm_resource_group" "rg" {
 
 # Create an Azure Container Registry for your images
 resource "azurerm_container_registry" "acr" {
-  count = var.deploy ? 1 : 0
+  count               = var.deploy ? 1 : 0
   name                = var.acr_name
   resource_group_name = var.resource_group_name
   location            = var.location
@@ -38,7 +38,7 @@ resource "azurerm_container_registry" "acr" {
 
 # Create an AKS Cluster
 resource "azurerm_kubernetes_cluster" "aks" {
-  count = var.deploy ? 1 : 0
+  count               = var.deploy ? 1 : 0
   name                = var.aks_cluster_name
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -56,33 +56,33 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   azure_active_directory_role_based_access_control {
     azure_rbac_enabled = true
-    admin_group_object_ids = [ azuread_group.entra_admin_group.object_id[0] ]
+    admin_group_object_ids = [ azuread_group.entra_admin_group[0].object_id ]
     tenant_id = data.azurerm_client_config.current.tenant_id
   }
-
 }
 
 resource "azurerm_role_assignment" "acrtoaks" {
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPush"
-  scope                            = azurerm_container_registry.acr.id
-  skip_service_principal_aad_check = true
+  count                              = var.deploy ? 1 : 0
+  principal_id                       = azurerm_kubernetes_cluster.aks[0].kubelet_identity[0].object_id
+  role_definition_name               = "AcrPush"
+  scope                              = azurerm_container_registry.acr[0].id
+  skip_service_principal_aad_check   = true
 }
 
 output "kube_config" {
-  value     = azurerm_kubernetes_cluster.aks.kube_config_raw
+  value     = var.deploy ? azurerm_kubernetes_cluster.aks[0].kube_config_raw : null
   sensitive = true
 }
 
 output "acr_login_server" {
-  value = azurerm_container_registry.acr.login_server[0]
+  value = var.deploy ? azurerm_container_registry.acr[0].login_server : null
 }
 
 output "acr_admin_username" {
-  value = azurerm_container_registry.acr.admin_username[0]
+  value = var.deploy ? azurerm_container_registry.acr[0].admin_username : null
 }
 
 output "acr_admin_password" {
-  value     = azurerm_container_registry.acr.admin_password[0]
+  value     = var.deploy ? azurerm_container_registry.acr[0].admin_password : null
   sensitive = true
 }
