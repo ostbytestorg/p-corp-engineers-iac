ea4c2744316e6ad0a7a1c4cf09bb7a761d346a62
mariusostby <mariusostby@sicra.no>
2025-04-25 13:57:23 +0200

    add kv

--- TF/FunctionApp/main.tf
+++ TF/FunctionApp/main.tf
@@ -33,6 +33,28 @@ resource "azurerm_service_plan" "function_app_service_plan" {
   sku_name            = "B1"
 }
 
+# Add Key Vault resource with RBAC mode enabled
+resource "azurerm_key_vault" "function_app_key_vault" {
+  name                        = var.key_vault_name
+  location                    = azurerm_resource_group.rg_function_app.location
+  resource_group_name         = azurerm_resource_group.rg_function_app.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+  sku_name                    = "standard"
+  
+  # Enable RBAC mode
+  enable_rbac_authorization = true
+}
+
+# Assign Key Vault Administrator role to the deploying principal
+resource "azurerm_role_assignment" "deployer_kv_admin" {
+  scope                = azurerm_key_vault.function_app_key_vault.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
 resource "azurerm_linux_function_app" "example_function_app" {
   name                          = var.function_app_name
   location                      = azurerm_resource_group.rg_function_app.location
@@ -54,6 +76,22 @@ resource "azurerm_linux_function_app" "example_function_app" {
   identity {
     type = "SystemAssigned"
   }
+
+  # Add an app setting to reference the key vault
+  app_settings = {
+    "KEYVAULT_ENDPOINT" = azurerm_key_vault.function_app_key_vault.vault_uri
+  }
+}
+
+# Assign Key Vault Secrets User role to the Function App's managed identity
+resource "azurerm_role_assignment" "function_app_kv_secrets_user" {
+  scope                = azurerm_key_vault.function_app_key_vault.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_linux_function_app.example_function_app.identity.0.principal_id
+
+  depends_on = [
+    azurerm_linux_function_app.example_function_app
+  ]
 }
 
 resource "azurerm_role_assignment" "function_app_role" {
--- TF/FunctionApp/terraform.tfvars
+++ TF/FunctionApp/terraform.tfvars
@@ -3,7 +3,6 @@ resource_group_name    = "rg-tf-functionapp"
 tags                   = { configuration = "terraform" }
 storage_account_name   = "sttfsicrafuncapp001"
 storage_container_name = "function-code"
-function_blob_name     = "function_app_code.zip"
-zip_file_path          = "./FUNCTION_APP_CODE.zip"
 service_plan_name      = "appplan-sicratffunc001"
 function_app_name      = "sicratffunc001"
+key_vault_name         = "sicratfkv001"
