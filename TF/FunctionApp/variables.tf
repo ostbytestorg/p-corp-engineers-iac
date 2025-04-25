d603b19dcdbf72404bc836ddc86a9f8eed768dc5
Marius Østby <51474245+mariusostby@users.noreply.github.com>
2025-04-25 14:02:23 +0200

    add kv (#42)

--- TF/FunctionApp/variables.tf
+++ TF/FunctionApp/variables.tf
@@ -1,44 +1,41 @@
-variable "location" {
-  description = "Azure region to deploy resources"
-  type        = string
-}
-
 variable "resource_group_name" {
-  description = "Name of the Azure resource group"
+  description = "The name of the resource group in which to create the function app."
   type        = string
 }
 
-variable "tags" {
-  description = "Tags to use on resources"
-  type        = map(string)
+variable "location" {
+  description = "The Azure Region in which to create the function app."
+  type        = string
 }
 
 variable "storage_account_name" {
-  description = "Name of the storage account for the function app"
+  description = "The name of the storage account."
   type        = string
 }
 
 variable "storage_container_name" {
-  description = "Name of the storage container for the function code"
+  description = "The name of the storage container."
   type        = string
-}
-
-variable "function_blob_name" {
-  description = "Blob name for the uploaded zipped function app code"
-  type        = string
-}
-
-variable "zip_file_path" {
-  description = "Path to the zipped function app code"
-  type        = string
+  default     = "function-code"
 }
 
 variable "service_plan_name" {
-  description = "Name of the App Service plan for the function app"
+  description = "The name of the service plan."
   type        = string
 }
 
 variable "function_app_name" {
-  description = "Name of the Linux Function App"
+  description = "The name of the function app."
+  type        = string
+}
+
+variable "tags" {
+  description = "A mapping of tags to assign to the resources."
+  type        = map(string)
+  default     = {}
+}
+
+variable "key_vault_name" {
+  description = "The name of the key vault."
   type        = string
 }
